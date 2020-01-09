1b80faff713b053d93aa01ba0d35d13a86fe8af9
Ankita-95 <59565174+Ankita-95@users.noreply.github.com>
2020-01-09 13:40:14 +0530

    Dependency Injection

---
+++ Addition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ankita
+{
+    class Addition : Calculator
+    {
+        double[] operands;
+        public Addition()
+        {
+
+        }
+
+        public Addition(double[] operands)
+        {
+            this.operands = operands;
+        }
+
+        public void SetOperands(double[] operands)
+        {
+            this.operands = operands;
+        }
+
+        public double Calculate()
+        {
+            double result = 0;
+            foreach(int operand in this.operands)
+            {
+                result = result + operand;
+            }
+            return result;
+        }
+    }
+}
